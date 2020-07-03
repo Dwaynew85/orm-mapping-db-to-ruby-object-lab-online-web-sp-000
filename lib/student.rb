c29c028b4e8b678cf9a7d636f976807a06668fb7
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-03 18:30:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,12 +54,13 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  self.all_students_in_grade_9
-  sql = <<-SQL
-  SELECT *
-  FROM students
-  WHERE grade = 9
-  SQL
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 9
+    SQL
 
-  DB[:conn].execute(sql).map {|student| self.new_from_db(studnt)}
+    DB[:conn].execute(sql).map {|student| self.new_from_db(studnt)}
+  end
 end
